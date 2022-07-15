dcc67d72c9648c8aa0e2a54dbcd5d55b904e0051
Ivan Alejandro Dominguez <39778949+DovaE@users.noreply.github.com>
2022-07-15 09:34:43 -0500

    Aplicación de notas
    En esta aplicación se emulo la aplicación de notas de los teléfonos iPhone mediante SwiftUI y MVVM

---
+++ ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  Notas
+//
+//  Created by Ivan Alejandro DOVA on 14/07/22.
+//
+
+import SwiftUI
+import CoreData
+
+struct ContentView: View {
+   
+    var body: some View {
+        Home()
+    }
+    
+}
+
---
+++ ViewModel.swift
@@ -0,0 +1,67 @@
+//
+//  ViewModel.swift
+//  Notas
+//
+//  Created by Ivan Alejandro DOVA on 14/07/22.
+//
+
+import Foundation
+import CoreData
+import SwiftUI
+
+class ViewModel: ObservableObject {
+    @Published var nota = ""
+    @Published var fecha = Date()
+    @Published var show = false
+    @Published var updateItem : Notas!
+    @Published var tipo = ""
+    // CoreData
+    
+    func saveData(context: NSManagedObjectContext){
+        let newNota = Notas(context: context)
+        newNota.nota = nota
+        newNota.fechas = fecha
+
+        do {
+            try context.save()
+            print("guardo")
+            show.toggle()
+        } catch let error as NSError {
+            // alerta al usario
+            print("No guardo", error.localizedDescription)
+        }
+    }
+    
+    func deleteData(item:Notas,context: NSManagedObjectContext){
+        context.delete(item)
+        //try! context.save()
+        do {
+            try context.save()
+            print("Elimino")
+        } catch let error as NSError {
+            // alerta al usario
+            print("No elimino", error.localizedDescription)
+        }
+    }
+    
+    func sendData(item: Notas){
+        updateItem = item
+        nota = item.nota ?? ""
+        fecha = item.fechas ?? Date()
+        show.toggle()
+    }
+    
+    func editData(context: NSManagedObjectContext){
+        updateItem.fechas = fecha
+        updateItem.nota = nota
+        do {
+            try context.save()
+            print("edito")
+            show.toggle()
+        } catch let error as NSError {
+            print("No edito", error.localizedDescription)
+        }
+    }
+    
+}
+
